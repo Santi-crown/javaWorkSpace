cfdefeba7c676013dfa88e4143bbf0d4a9f35599
santi-crown <samtigomusik@gmail.com>
2024-11-04 21:05:28 -0500

    Add technique for centering a frame on the screeen - long version

--- gui/FrameTest.java
+++ gui/FrameTest.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.*;
 
 public class FrameTest {
@@ -7,10 +9,31 @@ public class FrameTest {
         theFrame.setVisible(true);
         theFrame.setLocation(400,400);
 
-        // JButton button = new JButton("Press me");
-        // button.setSize(100, 100);
+        // Technique for centering a frame on the screeen
+        // First, obtain the size of the frame to be centered. Invoking
+        // the getSize method on a Component returns a Dimension
 
-        // theFrame.add(button);
+        Dimension frameSize = theFrame.getSize();
+
+        // Now, invoke the static getDefaultToolkit method on the
+        // Toolkit class to obtain a handle on the AWT Toolkit object
+        // for this platform, and then invoke its getScreenSize()
+        // method to obtain a second Dimension object.
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        // Compute the center point of the screen as (centerX, centerY).
+        int centerX = screenSize.width/2;
+        int centerY = screenSize.height/2;
+
+        // We want half of the frame to be to the left of center,
+        // and half to be above center.
+        int halfWidth = frameSize.width/2;
+        int halfHeight = frameSize.height/2;
+        theFrame.setLocation(centerX - halfWidth, centerY - halfHeight);
+
+        theFrame.setVisible(true);
+
 
     }
 }
