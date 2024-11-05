46cc079ac1a65a8bae30476f4a4960702e68ed79
santi-crown <samtigomusik@gmail.com>
2024-11-04 20:41:04 -0500

    First commit, begening of book code practive. Add gui directory, GUI JFrame add

---
+++ gui/FrameTest.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class FrameTest {
+    public static void main(String[] args){
+        JFrame theFrame = new JFrame("Whee!!");
+        theFrame.setSize(200,200);
+        theFrame.setVisible(true);
+    }
+}
