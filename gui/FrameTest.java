cfc1f6450e7df836cb1b2c86d9d9383f8aea6ab6
santi-crown <samtigomusik@gmail.com>
2024-11-04 20:53:45 -0500

    Add setLocation of the JFrame

--- gui/FrameTest.java
+++ gui/FrameTest.java
@@ -5,11 +5,12 @@ public class FrameTest {
         JFrame theFrame = new JFrame("Whee!!");
         theFrame.setSize(200,200);
         theFrame.setVisible(true);
+        theFrame.setLocation(400,400);
 
-        JButton button = new JButton("Press me");
-        button.setSize(100, 100);
+        // JButton button = new JButton("Press me");
+        // button.setSize(100, 100);
 
-        theFrame.add(button);
+        // theFrame.add(button);
 
     }
 }
